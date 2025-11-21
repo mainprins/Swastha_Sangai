98b8cf960159c802cf5cdc1c93269424bef826dc
Prince Bajgain <princebajagainprof@gmail.com>
2025-11-21 19:21:46 +0545

    Created some setup for prism ORM

---
+++ backend/prisma/migrations/20251121133248_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
